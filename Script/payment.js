ea73198d058afff7baf123f70ec94cb05c8d5064
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2022-08-25 22:16:55 +0530

    Updated Index file

--- Script/payment.js
+++
@@ -1,9 +0,0 @@
-let userData = JSON.parse(localStorage.getItem("userData")) 
-// console.log(userName)
-let display = (userData) =>{
-  userData.forEach(el => {
-    console.log(el.name)
-    document.getElementById('uName').innerText = `Hi, ${el.name}`
-  });
-}
-display(userData)
