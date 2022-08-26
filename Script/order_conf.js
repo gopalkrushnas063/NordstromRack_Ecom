77b7ff5d51bb0d44b04c45fe8c55807a90e708f4
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2022-08-26 23:50:40 +0530

    OTP,order confirmation page

---
+++ Script/order_conf.js
@@ -0,0 +1,126 @@
+particlesJS(
+  "particles-js",
+
+  {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 1299.3805191013182,
+        },
+      },
+      color: {
+        value: ["#5D47BA", "#FFDBFF", "#FB5435", "#E00A30", "#04CEF9"],
+      },
+      shape: {
+        type: "star",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 1,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 15,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: false,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "top",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+  }
+);
+
+let orderID = () => {
+  var digits = "0123456789";
+  let OTP = "";
+  for (let i = 0; i < 11; i++) {
+    OTP += digits[Math.floor(Math.random() * 10)];
+  }
+  let randomnum = document.getElementById('randomnum');;
+  randomnum.innerText = OTP;
+};
+
+orderID();
